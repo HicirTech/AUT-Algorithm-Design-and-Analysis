c708c88905a89c35c884eb09fbbde8b7e8d6a4f2
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-07-20 19:35:31 +1200

    fix some code, but tryFour still wrong
    1. add time on sureface pro4 M3 model
    2. add a wrong tryFour
    3. add number input on the Fibonacci class
    4. change display time message

--- src/lab1/Fibonacci.java
+++ src/lab1/Fibonacci.java
@@ -1,14 +1,18 @@
 package lab1;
 import static java.lang.System.nanoTime;
 import java.util.*;
-//import java.util.Stack;
+
 public class Fibonacci {
 
+	
 	private int number;
 	
-	Fibonacci(int number)
+	@SuppressWarnings("resource")
+	Fibonacci()
 	{
-		this.setNumber(number);
+		System.out.println("Please input a number for Fibonacci calculation");
+		this.setNumber(new Scanner(System.in).nextInt());
+		System.out.println("for number "+ this.getNumber());
 	}
 
 	public int getNumber() {
@@ -22,9 +26,10 @@ public class Fibonacci {
 	public void tryOne()
 	{
 		long start= nanoTime();
-		System.out.print("Here is try 1 - Number is : ");
+		System.out.print("Here is try 1 - Fibonacci  Number is : ");
 		System.out.println(tryOne(this.getNumber()));
-		System.out.println("Use "+(nanoTime()-start)+" nano-second\n");
+		System.out.println("On my computer, this use 9109515899 nano-second to calculate Fibonacci 45");
+		System.out.println("On your computer it use "+(nanoTime()-start)+" nano-second\n");
 	}
 	
 	private int tryOne(int number)
@@ -35,9 +40,10 @@ public class Fibonacci {
 	public void tryTwo()
 	{
 		long start= nanoTime();
-		System.out.print("Here is try 2 - Number is : ");
+		System.out.print("Here is try 2 - Fibonacci  Number is : ");
 		System.out.println(tryTwo(number));
-		System.out.println("Use "+(nanoTime()-start)+" nano-second\n");
+		System.out.println("On my computer, this use 64200 nano second to calculate Fibonacci 45");
+		System.out.println("On your computer it use "+(nanoTime()-start)+" nano-second\n");
 	}
 	
 	private int tryTwo(int number)
@@ -68,9 +74,10 @@ public class Fibonacci {
 	public void tryThree()
 	{
 		long start= nanoTime();
-		System.out.print("Here is try 3 - Number is : ");
+		System.out.print("Here is try 3 - Fibonacci  Number is : ");
 		System.out.println(tryThree(this.getNumber()));
-		System.out.println("Use "+(nanoTime()-start)+" nano-second\n");
+		System.out.println("On my computer, this use 317200 nano second to calculate Fibonacci 45");
+		System.out.println("On your computer it use "+(nanoTime()-start)+" nano-second\n");
 	}
 	private int tryThree(int number)
 	{
@@ -98,40 +105,62 @@ public class Fibonacci {
 	public void tryFour()
 	{
 		long start= nanoTime();
-		System.out.print("Here is try 4 - Number is : ");
+		System.out.print("Here is try 4: ");
 		System.out.println(tryFour(this.getNumber()));
-		System.out.println("Use "+(nanoTime()-start)+" nano-second\n");
+		System.out.println("On my computer, this use 927200 nano second to calculate Fibonacci 45");
+		System.out.println("On your computer it use "+(nanoTime()-start)+" nano-second\n");
+		System.out.println("The algorithm is wrong, try 4 will not get right answer!!!");
 	}
-	//--------------------------------------------------------------------------
 	private int tryFour(int number)
 	{
 		Stack<Integer> index =findIndexOfOne(number);
-		int arrayA[][] ={{0,1},{1,1}};
-		int arrayB[][] = {{1,0},{0,1}};
-
-		while(!index.isEmpty())
+		int arrayModel[][] = {{1,0},{0,1}};
+		int[][] resultArray;
+	
+		
+		int[][][] swap = new int[index.size()][2][2];
+		for(int i = 0; i != swap.length;i++)
 		{
-			int thisTime=index.pop().intValue();
-			System.out.println("thistime"+thisTime);
-			for(int inside =1;inside<=thisTime;inside++)
-			{
-				multMatrix(arrayB,arrayA);
-			}
+			swap[i]=arrayModel;
 		}
 		
-		return  arrayB[0][1];
+	
+		int swapInt = 0;
+		while(!index.isEmpty())
+		{
+			int thisTime = index.pop().intValue();
+			
+			for(int i=1;i<=thisTime;i++)
+			{
+				swap[swapInt]=matrixPowerUp(swap[swapInt]);
+			}
+			swapInt++;	
+		}
+	
+		resultArray = new int[2][2];
+		resultArray=returnMatrix(swap[0], swap[1]);
+		for(int i = 2;i!=swap.length;i++)
+		{			
+			resultArray=returnMatrix(resultArray,swap[i]);
+		}
+		return resultArray[0][1];
+		
+		
 	}
 	
 	private Stack<Integer> findIndexOfOne(int number)
 	{
 		String toBinary=Integer.toBinaryString(number).toString();
-		System.out.println(toBinary);
+		System.out.println("Binary is :"+toBinary);
+		System.out.print("index of 1 is : ");
 		Stack<Integer> index = new Stack<Integer>();
 		while(toBinary.contains("1"))
 		{	
 			index.add(toBinary.length()-toBinary.indexOf("1"));
 			toBinary=toBinary.replaceFirst("1", "0");
 		}
+		System.out.println(index.toString());
+		
 		return index;
 	}
 	private void multMatrix(int[][] arrayB, int [][] arrayA) {
@@ -144,4 +173,20 @@ public class Fibonacci {
         arrayB[1][0] = c;
         arrayB[1][1] = d;
     }
+	private int[][] returnMatrix(int[][] arrayB, int [][] arrayA) {
+        int a = arrayB[0][0] * arrayA[0][0] +  arrayB[0][1] * arrayA[1][0];
+        int b = arrayB[0][0] * arrayA[0][1] +  arrayB[0][1] * arrayA[1][1];
+        int c = arrayB[1][0] * arrayA[0][0] +  arrayB[1][1] * arrayA[0][1];
+        int d = arrayB[1][0] * arrayA[0][1] +  arrayB[1][1] * arrayA[1][1];
+        int result[][] = {{a,b},{c,d}};
+        return result;
+    }
+	private int[][] matrixPowerUp(int[][] arrayB) {
+        int a = arrayB[0][0] * 0 +  arrayB[0][1] * 1;
+        int b = arrayB[0][0] * 1 +  arrayB[0][1] * 1;
+        int c = arrayB[1][0] * 0 +  arrayB[1][1] * 1;
+        int d = arrayB[1][0] *1 +  arrayB[1][1] * 1;
+        int result[][] = {{a,b},{c,d}};
+        return result;
+        }
 }
--- src/lab1/Runable.java
+++ src/lab1/Runable.java
@@ -1,11 +1,14 @@
 package lab1;
-
+import java.util.Scanner;
 public class Runable {
 
 	public static void main(String[] args) {
 		
-		Fibonacci fibonacci = new Fibonacci(45);
-	
+		Fibonacci fibonacci = new Fibonacci();
+		fibonacci.tryOne();
+		fibonacci.tryTwo();
+		fibonacci.tryThree();
+		System.out.println("ERROR IN TRY FOUR");
 		fibonacci.tryFour();
 		
 	}
