9572e8d3cc0089e98693f9e15560ca9f699fd4e3
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-07-29 12:37:23 +1200

    Lab2 code update
    add lab2 code also commented unused code

--- Week2/Lab2/Runable.java
+++ Week2/Lab2/Runable.java
@@ -1,16 +1,27 @@
 package Lab2;
+
+import static java.lang.System.nanoTime;
+
+import java.util.Scanner;
 /**
- * 
- * @author luoze
- *
+ * this the lab 2 Question 3 runnable part, require user input array size and the number to look up
+ * Warning: A large size array may course stack overflow
+ * nano-second time will be show after method ran
+ * @author Luo Zeting ID:16938158
+ * @see https://github.com/HicirTech/AUT-ADA
  */
 public class Runable {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Karatsuba a  = new Karatsuba();
-		System.out.println(a.multiply(11,11));
-	//	System.out.println(a.splitNumberIntoBinary(3));
-	}
-
+		System.out.println("Please input number of size of the array  Warning: A large size array may course stack overflow");
+		int [] array = new int[new Scanner(System.in).nextInt()];
+		for(int i = 0; i!=array.length;i++)
+			array[i]=i;		
+		System.out.println("Please input a number to look for:");
+		lab2 a = new lab2(array,new Scanner(System.in).nextInt());
+	
+		long start = nanoTime();//Start counting
+		System.out.println("found? "+a.lookFor()+"   Used time "+ (nanoTime()-start)+" nano-second");}
+		
 }
---
+++ Week2/Lab2/lab2.java
@@ -0,0 +1,41 @@
+package Lab2;
+
+import java.util.ArrayList;
+/**
+ * this is the lab2 Question 3.2, this class will take a sorted array, and a value to lookup. 
+ * a index will be randomly generated, it will not repeat, a index will only be search onece
+ * @author Luo Zeting ID:16938158
+ * @see  https://github.com/HicirTech/AUT-ADA
+ */
+public class lab2 {
+	private int[] array;
+	private int value;
+	private ArrayList<Integer> repeat;
+	lab2(int[] array, int value)
+	{
+		this.array=array;
+		this.value=value;
+		this.repeat=new ArrayList<>();
+	}
+	public boolean lookFor() {
+		int i = getRamdom(); // get a ramdom not repeat index 
+		repeat.add(i); //  add it to repeat list, so that this index will not show up again
+						// it is equal to search in the subArray
+		return (array[i]==value)?true:(repeat.size()==array.length)?false:lookFor();
+							// if value found return true
+							// if value not found in the array return false
+							// if value not found and it is not the end of the array
+							//     go to next index lookfor
+	}		
+	
+	private int getRamdom()
+	{
+		int i;
+		do {
+			i= (int) (Math.random()*array.length);
+		}while(repeat.contains(i)==true);
+		return i;
+	}
+	
+}
+
